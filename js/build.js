691da527c919edba92a5bd913663ebe86ba9f5b7
kolastar2016 <kolastarsky@gmail.com>
2018-07-07 01:34:47 +0300

    work

---
+++ js/build.js
@@ -0,0 +1,2 @@
+$('.carousel').carousel()
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm1haW4uanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUEiLCJmaWxlIjoiYnVpbGQuanMiLCJzb3VyY2VzQ29udGVudCI6WyIkKCcuY2Fyb3VzZWwnKS5jYXJvdXNlbCgpIl19
